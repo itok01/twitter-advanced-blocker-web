36b8eca8fce89cfa92aa1d5f5bbf8dcf9bd5f443
itok01 <contact@itok01.com>
2020-03-19 23:00:14 +0900

    Add login button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,21 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@material-ui/core';
+
+import styles from '../styles/Header.module.css'
 
 const title = "Advanced Blocker";
+const authUrl = "https://localhost/api/auth";
 
 export default function Header() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography component="h1" className="title" variant="h6">
+                <Typography component="h1" variant="h6" className={styles.title}>
                     {title}
                 </Typography>
+                <Button variant="text" color="inherit" onClick={() => window.open(authUrl, "_self")}>
+                    ログイン
+                </Button>
             </Toolbar>
         </AppBar>
     )
