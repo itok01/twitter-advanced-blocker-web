d7ea4cdd14f27e2c35595de57036d0d6fb1c6113
itok01 <contact@itok01.com>
2020-04-14 17:34:21 +0900

    Add /blocklist link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { NextPageContext, NextPage } from 'next';
 import { parseCookies } from 'nookies'
-import { Link } from '@material-ui/core';
+import { Link, Typography } from '@material-ui/core';
 
 import { Auth } from '../lib/auth';
+import { getBlocklist } from '../lib/blocklist';
 import Layout from '../components/MyLayout'
 
 type Props = {
@@ -12,24 +14,27 @@ type Props = {
 const IndexPage: NextPage<Props> = props => {
     return (
         <Layout auth={props.auth} >
-            <h1>Hello world!</h1>
-            <Link href="https://localhost/blocklist">
-                ブロックリスト
-            </Link>
+            <Typography component="h2" variant="h6">
+                <Link href="/blocklist" underline="none">
+                    ブロックリスト
+                </Link>
+            </Typography>
         </Layout>
     )
 };
 
 IndexPage.getInitialProps = async (ctx: NextPageContext) => {
-    let props: Props = { auth: { token: "", signedIn: false } };
-
     const cookies = parseCookies(ctx);
 
+    let auth: Auth = { token: "", signedIn: false };
+
     if (cookies["oauth_token"] != undefined && cookies["oauth_token"] != "") {
-        props.auth.token = cookies["oauth_token"];
-        props.auth.signedIn = true;
+        auth.token = cookies["oauth_token"];
+        auth.signedIn = true;
     }
-    return props;
+    const blocklistResponse = await getBlocklist(auth.token);
+
+    return { auth: auth };
 }
 
 export default IndexPage;
